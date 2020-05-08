eedda0e4ad9c9f7426141170e187730044aceb80
Fadila Eka Noperta <fadilaekanoperta@gmail.com>
2020-05-08 10:19:26 +0700

    Router-Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
-import React , {Component} from 'react';
+import React , {Suspense} from 'react';
 import AppNavbar from './components/Navbar';
+import { Route, Switch } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
 
+//pages
+import HomePage from "./components/views/HomePage.js";
+import KatalogPage from "./components/views/KatalogPage.js";
+import PromoPage from "./components/views/PromoPage.js";
+import AboutPage from "./components/views/AboutPage.js";
+
 function App() {
   return (
-    <div className="App">
-      <AppNavbar/>
-    </div>
+    //<div className="App">
+      //<AppNavbar/>
+    //</div>
     
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <AppNavbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/katalog" component={KatalogPage} />
+        <Route exact path="/promo" component={PromoPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Switch>
+    </Suspense>
   );
 }
 
