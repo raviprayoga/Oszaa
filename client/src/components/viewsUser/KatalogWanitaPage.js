cd2604bf71ce76cf9968d1d25fad7bc95f289102
MuminatulFaizah <muminatulfaizah25@gmail.com>
2020-05-12 02:29:02 +0700

    Update KatalogWanitaPage.js

--- client/src/components/viewsUser/KatalogWanitaPage.js
+++ client/src/components/viewsUser/KatalogWanitaPage.js
@@ -149,6 +149,46 @@ class KatalogWanitaPage extends React.Component{
                         </div>
                         </Col>
                     </Row>
+
+                    <Pagination className="pagination">
+                        <PaginationItem disabled>
+                            <PaginationLink first href="#" />
+                        </PaginationItem>
+                        <PaginationItem disabled>
+                            <PaginationLink previous href="#" />
+                        </PaginationItem>
+                        <PaginationItem active>
+                            <PaginationLink href="#">
+                            1
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            2
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            3
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            4
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink href="#">
+                            5
+                            </PaginationLink>
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink next href="#" />
+                        </PaginationItem>
+                        <PaginationItem>
+                            <PaginationLink last href="#" />
+                        </PaginationItem>
+                    </Pagination>
                 </Container>
             </div>
         );
